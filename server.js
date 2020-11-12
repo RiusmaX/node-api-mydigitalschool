2367cb69c0bb2d4e8f066ce6fd268772729f5e9a
Marius <cacahouette72@gmail.com>
2020-11-12 12:17:55 +0100

    add router

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
+var router = express.Router()
 
-const server = http.createServer((req, res) => {
-    console.log(req);
-    res.end('Voilà la réponse du serveur !');
-});
+app.use(router)
 
-server.listen(process.env.PORT || 3000);
+app.use('/', require('./routes'))
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
