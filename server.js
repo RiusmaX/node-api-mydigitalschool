cb642d721abf5fb1b17b7702959728ee21877c04
Marius <cacahouette72@gmail.com>
2020-11-12 14:02:46 +0100

    add env props

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+
 const express = require('express')
 const Logger = require('./tools/logger')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 var router = express.Router()
 
 app.use(Logger)
