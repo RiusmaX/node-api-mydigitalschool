f77eb0d80daf972f5caace8866a9b9faed65cd9c
Marius <cacahouette72@gmail.com>
2020-11-12 15:06:14 +0100

    add the post call to add a note

--- routes/notes/index.js
+++ routes/notes/index.js
@@ -26,8 +26,36 @@ var notes = [
 
 // Nous sommes déjà dans /notes
 router.route('/') // = localhost:PORT/notes/
-.get((req, res) => {
+.get((req, res) => { // Récupération de la liste de notes
+    // On répond avec la liste des notes
     res.send(notes)
+})
+.post((req, res) => {
+    console.log('body', req.body)
+    // On récupère nos variables envoyée par le client
+    const title = req.body.title
+    const description = req.body.description
+
+    // On teste si la data reçue n'est pas nulle 
+    if (!title) {
+        res.status(500).send('Le titre est manquant')
+    } else if (!description) {
+        res.status(500).send('La description est manquante')
+    } else {
+        // La data est OK
+        
+        // On ajoute la note à la liste
+        notes.push({
+            id: Math.random().toString(36).substr(2, 9),
+            title: title,
+            description: description,
+            isEnabled: true,
+            isFavorite: false
+        })
+
+        // On envoit la liste mise à jour
+        res.send(notes)
+    }
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,19 +1,33 @@
+// Initialisation de l'environnement
 require('dotenv').config()
 
+// Imports
 const express = require('express')
+const bodyParser = require('body-parser')
 const Logger = require('./tools/logger')
 
 const app = express()
+
+// Réglage du port d'écoute
 const port = process.env.PORT || 3000
 var router = express.Router()
 
+// Mise en place du bodyParser afin d'obtenir un objet req.body
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Mise en place du logger
 app.use(Logger)
 
 app.use(router)
 
+/**
+ * Routes
+ */
 app.use('/', require('./routes'))
 app.use('/notes', require('./routes/notes'))
 
+// Start server listening
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
