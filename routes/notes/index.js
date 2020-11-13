fb74872811dc7552908831e56f2f80a110c5cc7e
Marius <cacahouette72@gmail.com>
2020-11-13 11:39:24 +0100

    add put and delete

--- routes/notes/index.js
+++ routes/notes/index.js
@@ -3,16 +3,25 @@ const router = require('express').Router()
 // Import du modèle Mongoose Note
 const Note = require('../../models/note')
 
+// Fonction globale de récupération des notes
+findNotes = () => {
+    return new Promise((resolve, reject) => {
+        Note.find((error, notes) => {
+            if (error) {
+                reject(error)
+            } else {
+                resolve(notes)
+            }
+        })
+    })
+}
+
 // Nous sommes déjà dans /notes
 router.route('/') // = localhost:PORT/notes/
 .get((req, res) => { // Récupération de la liste de notes
-    Note.find((error, notes) => {
-        if (error) {
-           return res.status(500).send(error)
-        }
-        // On répond avec la liste des notes
-        return res.send(notes)
-    })
+    findNotes()
+    .then(notes => res.send(notes))
+    .catch(error => res.status(500).send(error))
 })
 .post((req, res) => { // Insertion d'un nouvel élément
     // On récupère nos variables envoyée par le client
@@ -21,9 +30,9 @@ router.route('/') // = localhost:PORT/notes/
 
     // On teste si la data reçue n'est pas nulle 
     if (!title) {
-        res.status(500).send('Le titre est manquant')
+        return res.status(500).send('Le titre est manquant')
     } else if (!description) {
-        res.status(500).send('La description est manquante')
+        return res.status(500).send('La description est manquante')
     } else {
         // La data est OK
         const note = new Note()
@@ -35,16 +44,12 @@ router.route('/') // = localhost:PORT/notes/
             // Traitement des erreurs
             if (error) {
                 return res.status(500).send(error)
+            } else {
+                // Récupération de la liste des notes
+                findNotes()
+                .then((notes) => res.send(notes))
+                .catch((error) => res.status(500).send(error))
             }
-
-            // Récupération de la liste des notes
-            Note.find((error, notes) => {
-                if (error) {
-                    return res.status(500).send(error)
-                }
-                // On envoit la liste des notes
-                return res.send(notes)
-            })
         })
     }
 })
@@ -53,20 +58,48 @@ router.route('/') // = localhost:PORT/notes/
     const id = req.body.id
 
     if (!id) {
-        res.status(500).send('L\'id est manquant')
+        return res.status(500).send('L\'id est manquant')
     } else {
-        // On recherche dans le tableau l'index de l'objet possédant l'id envoyé en paramètre
-    
+        Note.findByIdAndDelete(id, (error, notes) => {
+            if (error) {
+                return res.status(500).send(error)
+            } else {
+                findNotes()
+                .then((notes) => res.send(notes))
+                .catch((error) => res.status(500).send(error))
+            }
+        })
     }
 })
 .put((req, res) => { // Mise à jour d'un élément par son ID
-    // On récupère l'ID de la note à mettre à jour
+    // On récupère l'ID de la note à mettre à jour et les paramètres
     const id = req.body.id
+    const title = req.body.title
+    const description = req.body.description
+    const isEnabled = req.body.isEnabled
+    const isFavorite = req.body.isFavorite
 
     if (!id) {
         res.status(500).send('L\'id est manquant')
     } else {
-       
+        // On créé un nouvel objet node
+        const _note = {
+            // title: title,
+            title,
+            description,
+            isEnabled,
+            isFavorite
+        }
+        // On met à jour l'objet note
+        Note.findByIdAndUpdate(id, _note, (error, notes) => {
+            if (error) {
+                return res.status(500).send(error)
+            } else {
+                findNotes()
+                .then((notes) => res.send(notes))
+                .catch((error) => res.status(500).send(error))
+            }
+        })
     }
 })
 
